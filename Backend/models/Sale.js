58910eeb36ae37c8361af57f5bdd8698155231f6
PuiiNuii <hengthaingoun7@gmail.com>
2025-03-29 03:24:31 +0700

    DL

---
+++ Backend/models/Sale.js
@@ -0,0 +1,96 @@
+// const mongoose = require('mongoose');
+
+// const saleSchema = new mongoose.Schema({
+//   functionNo: { type: Number, required: true },
+//   customerName: { type: String, required: true },
+//   iceTypeOriginal: { type: Number, default: 0 },
+//   unitPriceOriginal: { type: Number, default: 0 },
+//   iceTypeLarge: { type: Number, default: 0 },
+//   unitPriceLarge: { type: Number, default: 0 },
+//   iceTypeSmall: { type: Number, default: 0 },
+//   unitPriceSmall: { type: Number, default: 0 },
+//   oldDebt: { type: Number, default: 0 },
+//   newDebt: { type: Number, default: 0 },
+//   payment: { type: Number, default: 0 },
+//   expenses: { type: Number, default: 0 },
+//   date: { type: Date, default: Date.now }
+// }, {
+//   toJSON: { virtuals: true },
+//   toObject: { virtuals: true }
+// });
+
+// // Virtual properties for calculated fields
+// saleSchema.virtual('revenueOriginal').get(function() {
+//   return this.iceTypeOriginal * this.unitPriceOriginal;
+// });
+
+// saleSchema.virtual('revenueLarge').get(function() {
+//   return this.iceTypeLarge * this.unitPriceLarge;
+// });
+
+// saleSchema.virtual('revenueSmall').get(function() {
+//   return this.iceTypeSmall * this.unitPriceSmall;
+// });
+
+// saleSchema.virtual('totalRevenue').get(function() {
+//   return this.revenueOriginal + this.revenueLarge + this.revenueSmall;
+// });
+
+// saleSchema.virtual('totalDebt').get(function() {
+//   return this.oldDebt + this.newDebt - this.payment;
+// });
+
+// saleSchema.virtual('netIncome').get(function() {
+//   return this.totalRevenue - this.expenses;
+// });
+
+// module.exports = mongoose.model('Sale', saleSchema);
+
+
+
+const mongoose = require('mongoose');
+
+const saleSchema = new mongoose.Schema({
+  customerName: { type: String, required: true },
+  saleDate: { type: Date, required: true, default: Date.now },
+  iceTypeOriginal: { type: Number, default: 0 },
+  unitPriceOriginal: { type: Number, default: 0 },
+  iceTypeLarge: { type: Number, default: 0 },
+  unitPriceLarge: { type: Number, default: 0 },
+  iceTypeSmall: { type: Number, default: 0 },
+  unitPriceSmall: { type: Number, default: 0 },
+  oldDebt: { type: Number, default: 0 },
+  newDebt: { type: Number, default: 0 },
+  payment: { type: Number, default: 0 },
+  expenses: { type: Number, default: 0 }
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
+});
+
+// Virtual properties for calculated fields
+saleSchema.virtual('revenueOriginal').get(function() {
+  return this.iceTypeOriginal * this.unitPriceOriginal;
+});
+
+saleSchema.virtual('revenueLarge').get(function() {
+  return this.iceTypeLarge * this.unitPriceLarge;
+});
+
+saleSchema.virtual('revenueSmall').get(function() {
+  return this.iceTypeSmall * this.unitPriceSmall;
+});
+
+saleSchema.virtual('totalRevenue').get(function() {
+  return this.revenueOriginal + this.revenueLarge + this.revenueSmall;
+});
+
+saleSchema.virtual('totalDebt').get(function() {
+  return this.oldDebt + this.newDebt - this.payment;
+});
+
+saleSchema.virtual('netIncome').get(function() {
+  return this.totalRevenue - this.expenses;
+});
+
+module.exports = mongoose.model('Sale', saleSchema);
---
+++ Frontend/dist/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php include "_head.php"?>
+
+  <body>
+    <div class="container-scroller">
+      <div class="row p-0 m-0 proBanner" id="proBanner">
+        <div class="col-md-12 p-0 m-0">
+          <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
+            <div class="ps-lg-1">
+              <div class="d-flex align-items-center justify-content-between">
+                <p class="mb-0 font-weight-medium me-3 buy-now-text">ការគាំទ្រអតិថិជន 24/7 ឥតគិតថ្លៃ</p>
+                <a href="https://t.me/Ear_Sokchan" target="_blank" class="btn me-2 buy-now-btn border-0">ទាក់ទងឥឡូវនេះ</a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-between">
+              <a href="https://t.me/Ear_Sokchan"><i class="icon-home me-3 text-white"></i></a>
+              <button id="bannerClose" class="btn border-0 p-0">
+                <i class="icon-close text-white me-0"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- partial:partials/_navbar.html -->
+
+      <?php include "_navbar.php"?>
+
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+        
+
+        <?php include "_sidebar.php"?>
+
+        <?php include "_endinject.php"?>
+        <!-- End custom js for this page -->
+  </body>
+</html>
